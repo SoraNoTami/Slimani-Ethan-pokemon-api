e2459c23f41e7d20a6da33b5d6d8acaca94e112d
Sora <92116172+SoraNoTami@users.noreply.github.com>
2023-05-17 11:33:20 +0200

    Finish Project

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,13 +1,30 @@
-import React  from "react";
+import React, {useState, useEffect, useReducer} from "react";
+import axios from "axios";
 
-function PokemonList({ searchTerm = "" }) {
+function PokemonList({ searchTerm }) {
 
+  const [List, setList] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=50");
+    console.log("res", res.data.results);
+    setList(res.data.results);
+  }
 
   return (
+    <>
     <ul>
-    <li>test</li>
-    <li>test2</li>
+    {List.map((pokemon) => {
+      return (
+        <li>{pokemon.name}</li>
+      )
+    }) }
     </ul>
+    </>
   );
 }
 
